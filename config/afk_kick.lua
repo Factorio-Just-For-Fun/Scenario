785f01ffa5c0af940cd36c8a51671fd60e9b418a
Username4723 <11415806+Username4723@users.noreply.github.com>
2025-01-18 10:57:55 -0600

    AFK kick fix 2

--- config/afk_kick.lua
+++ config/afk_kick.lua
@@ -1,6 +1,6 @@
 return {
-    admin_as_active = true, --- @setting admin_as_active When true admins will be treated as active regardless of afk time
-    trust_as_active = true, --- @setting trust_as_active When true trusted players (by playtime) will be treated as active regardless of afk time
+    admin_as_active = false, --- @setting admin_as_active When true admins will be treated as active regardless of afk time
+    trust_as_active = false, --- @setting trust_as_active When true trusted players (by playtime) will be treated as active regardless of afk time
     active_role = nil, --- @setting active_role When not nil a player with this role will be treated as active regardless of afk time
     afk_time = 3600*10, --- @setting afk_time The time in ticks that must pass for a player to be considered afk
     kick_time = 3600*30, --- @setting kick_time The time in ticks that must pass without any active players for all players to be kicked
